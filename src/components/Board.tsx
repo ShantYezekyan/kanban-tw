77444cfc86f074bf4917f033d69309bf55dc8cc5
ShantYezekyan <shant.yezekyan@gmail.com>
2024-02-13 06:07:45 +0400

    add functionality to reorder with dnd within same column

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -3,11 +3,45 @@ import { DragDropContext } from "react-beautiful-dnd";
 import { Column, DeleteBox } from ".";
 import { DEFAULT_CARDS } from "../MOCK_DATA";
 import type { BoardData } from "../MOCK_DATA";
+import type { DropResult } from "react-beautiful-dnd";
 
 const Board = () => {
   const [data, setData] = useState<BoardData>(DEFAULT_CARDS);
 
-  const handleDragEnd = () => {};
+  const handleDragEnd = (result: DropResult) => {
+    const { destination, source, draggableId } = result;
+
+    // Checked if dropped outside of droppable area
+    if (!destination) {
+      return;
+    }
+
+    //Check if dropped in the same column
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    const column = data.columns[source.droppableId];
+    const newTaskIds = [...column.taskIds];
+    newTaskIds.splice(source.index, 1);
+    newTaskIds.splice(destination.index, 0, draggableId);
+
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds,
+    };
+
+    setData({
+      ...data,
+      columns: {
+        ...data.columns,
+        [newColumn.id]: newColumn,
+      },
+    });
+  };
 
   return (
     <DragDropContext onDragEnd={handleDragEnd}>
