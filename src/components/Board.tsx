f766b222250dbb6893963af3c9b17c3931fbd687
ShantYezekyan <shant.yezekyan@gmail.com>
2024-02-13 04:34:24 +0400

    refactor card drag and drop with react-beautiful-dnd

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,45 +1,50 @@
 import { useState } from "react";
 import { CardData, DEFAULT_CARDS } from "../MOCK_DATA";
 import { Column, DeleteBox } from ".";
+import { DragDropContext } from "react-beautiful-dnd";
 
 const Board = () => {
   const [cards, setCards] = useState<CardData[]>(DEFAULT_CARDS);
 
-  return (
-    <div className="flex h-full w-full flex-col p-12">
-      <div className="flex h-full w-full gap-3 overflow-scroll">
-        <Column
-          title="Backlog"
-          column="backlog"
-          headingColor="text-neutral-500"
-          cards={cards}
-          setCards={setCards}
-        />
-        <Column
-          title="TODO"
-          column="todo"
-          headingColor="text-yellow-200"
-          cards={cards}
-          setCards={setCards}
-        />
-        <Column
-          title="In progress"
-          column="in-"
-          headingColor="text-blue-200"
-          cards={cards}
-          setCards={setCards}
-        />
-        <Column
-          title="Complete"
-          column="done"
-          headingColor="text-emerald-200"
-          cards={cards}
-          setCards={setCards}
-        />
-      </div>
+  const handleDragEnd = () => {};
 
-      <DeleteBox setCards={setCards} />
-    </div>
+  return (
+    <DragDropContext onDragEnd={handleDragEnd}>
+      <div className="flex h-full w-full flex-col p-12">
+        <div className="flex h-full w-full gap-3 overflow-scroll">
+          <Column
+            title="Backlog"
+            column="backlog"
+            headingColor="text-neutral-500"
+            cards={cards}
+            setCards={setCards}
+          />
+          <Column
+            title="TODO"
+            column="todo"
+            headingColor="text-yellow-200"
+            cards={cards}
+            setCards={setCards}
+          />
+          <Column
+            title="In progress"
+            column="in-progress"
+            headingColor="text-blue-200"
+            cards={cards}
+            setCards={setCards}
+          />
+          <Column
+            title="Complete"
+            column="done"
+            headingColor="text-emerald-200"
+            cards={cards}
+            setCards={setCards}
+          />
+        </div>
+
+        <DeleteBox setCards={setCards} />
+      </div>
+    </DragDropContext>
   );
 };
 
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,7 @@
 import Board from "./Board";
 import Column from "./Column";
 import Card from "./Card";
-import DropIndicator from "./DropIndicator";
 import AddCard from "./AddCard";
 import DeleteBox from "./DeleteBox";
 
-export { Board, Column, Card, DropIndicator, AddCard, DeleteBox };
+export { Board, Column, Card, AddCard, DeleteBox };
